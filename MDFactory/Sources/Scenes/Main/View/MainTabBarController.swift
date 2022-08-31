350aaddb60ad9855ec33a3ce75cc640452de16e9
Vadim <vadim_kimm@icloud.com>
2022-08-31 19:04:04 +0300

    Get rid off main.storyboard

---
+++ MDFactory/Sources/Scenes/Main/View/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarController.swift
+//  MDFactory
+//
+//  Created by Vadim Kim on 31.08.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
