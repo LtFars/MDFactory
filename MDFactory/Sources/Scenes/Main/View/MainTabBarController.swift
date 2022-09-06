aeb4f8095a4670570d22982615e9f3a6f904198f
alexey-z-m <alexey.panda@gmail.com>
2022-09-06 09:36:56 +0300

    mainPage
    created tabBar and collectionView on mainPage

--- MDFactory/Sources/Scenes/Main/View/MainTabBarController.swift
+++ MDFactory/Sources/Scenes/Main/View/MainTabBarController.swift
@@ -1,29 +1,34 @@
-//
-//  MainTabBarController.swift
-//  MDFactory
-//
-//  Created by Vadim Kim on 31.08.2022.
-//
-
 import UIKit
 
 class MainTabBarController: UIViewController {
-
+    private let _tabBarController = UITabBarController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .blue
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        setupTabBar()
     }
-    */
-
+    
+    func setupTabBar() {
+        _tabBarController.tabBar.tintColor = UIColor(hex: "#6A4CE2FF")
+        let main = UINavigationController(rootViewController: MainPage())
+        main.tabBarItem.image = UIImage(systemName: "person")
+        main.title = "Main"
+        let course = UINavigationController(rootViewController: MainPage())
+        course.tabBarItem.image = UIImage(systemName: "person")
+        course.title = "Course"
+        let lesson = UINavigationController(rootViewController: MainPage())
+        lesson.tabBarItem.image = UIImage(systemName: "person")
+        lesson.title = "Lesson"
+        let testing = UINavigationController(rootViewController: MainPage())
+        testing.tabBarItem.image = UIImage(systemName: "person")
+        testing.title = "Testing"
+        let views = [main, course, lesson, testing]
+        _tabBarController.setViewControllers(views, animated: true)
+                _tabBarController.modalPresentationStyle = .fullScreen
+        present(_tabBarController, animated: false)
+    }
 }
