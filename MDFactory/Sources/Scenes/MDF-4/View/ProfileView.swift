6a62d6fe35b42abd29fc277a8976f7b4c0887c24
Andrik <masmay.pl@gmail.com>
2022-09-10 13:26:31 +0200

    edit constrait snapKit

--- MDFactory/Sources/Scenes/MDF-4/View/ProfileView.swift
+++ MDFactory/Sources/Scenes/MDF-4/View/ProfileView.swift
@@ -6,10 +6,23 @@
 //
 
 import UIKit
-
+import SnapKit
 
 class ProfileView: UIView {
     
+    // Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupHierarchy()
+        addStackView()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Properties
     private lazy var avatarImageView: UIImageView = {
         var imageView = UIImageView()
         let image = UIImage(named: "emoji")
@@ -28,15 +41,10 @@ class ProfileView: UIView {
     private lazy var sheetImageView: UIImageView = {
         var imageView = UIImageView()
         imageView.backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9647058824, blue: 0.9882352941, alpha: 1)
+
         imageView.clipsToBounds = true
         imageView.layer.masksToBounds = true
         imageView.layer.cornerRadius = 40
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
-        var someView: ProgressView = {
-        var imageView = ProgressView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
@@ -49,12 +57,19 @@ class ProfileView: UIView {
         return imageView
     }()
     
+    private lazy var centerImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(systemName: "chevron.down")
+        imageView.tintColor = .black
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     private lazy var nameLabel: UILabel = {
         var name = UILabel()
         name.text = "Robert Jakson"
         name.font = .systemFont(ofSize: 21, weight: .heavy)
         name.font = .preferredFont(forTextStyle: .headline)
-        
         name.translatesAutoresizingMaskIntoConstraints = false
         return name
     }()
@@ -67,14 +82,42 @@ class ProfileView: UIView {
         return name
     }()
     
-    private lazy var centerImageView: UIImageView = {
-        var imageView = UIImageView()
-        imageView.image = UIImage(systemName: "chevron.down")
-        imageView.tintColor = .black
+    private lazy var userInfoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var centerLabelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var percentSpeaking = setupPercentLabel(percent: "14%")
+    private lazy var percentListening = setupPercentLabel(percent: "46%")
+    private lazy var percentReading = setupPercentLabel(percent: "65%")
+    
+    private lazy var nameSpeaking = setupNamePercentLabel(percent: "Speaking",
+                                                          color: Color.red.color)
+    private lazy var nameListening = setupNamePercentLabel(percent: "Listening",
+                                                           color: Color.blue.color)
+    private lazy var nameReading = setupNamePercentLabel(percent: "Reading",
+                                                         color: Color.green.color)
+    // Custom properties
+        var someView: ProgressView = {
+        var imageView = ProgressView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-
+    
+    // Metods
     private func setupPercentLabel(percent: String) -> UILabel {
         let name = UILabel()
         name.text = percent
@@ -93,33 +136,18 @@ class ProfileView: UIView {
         return name
     }
     
-    private lazy var percentSpeaking = setupPercentLabel(percent: "14%")
-    private lazy var percentListening = setupPercentLabel(percent: "46%")
-    private lazy var percentReading = setupPercentLabel(percent: "65%")
-    
-    private lazy var nameSpeaking = setupNamePercentLabel(percent: "Speaking",
-                                                          color: Color.red.color)
-    private lazy var nameListening = setupNamePercentLabel(percent: "Listening",
-                                                           color: Color.blue.color)
-    private lazy var nameReading = setupNamePercentLabel(percent: "Reading",
-                                                         color: Color.green.color)
-    
-    
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupHierarchy()
-        setupLayout()
+    private func addStackView() {
         
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        userInfoStackView.addArrangedSubview(avatarImageView)
+        userInfoStackView.addArrangedSubview(nameLabel)
+        
+        centerLabelStackView.addArrangedSubview(centerLabel)
+        centerLabelStackView.addArrangedSubview(centerImageView)
     }
     
     private func setupHierarchy() {
-        addSubview(avatarImageView)
-        addSubview(nameLabel)
+        addSubview(userInfoStackView)
+        addSubview(centerLabelStackView)
         addSubview(sheetImageView)
         addSubview(stripImageView)
         addSubview(someView)
@@ -129,83 +157,71 @@ class ProfileView: UIView {
         addSubview(nameSpeaking)
         addSubview(nameListening)
         addSubview(nameReading)
-        addSubview(centerLabel)
-        addSubview(centerImageView)
-        
     }
     
     private func setupLayout() {
         
-        NSLayoutConstraint.activate([
-            avatarImageView.bottomAnchor.constraint(equalTo: topAnchor, constant: 100),
-            avatarImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            avatarImageView.heightAnchor.constraint(equalToConstant: 170),
-            avatarImageView.widthAnchor.constraint(equalToConstant: 170)
-        ])
+        userInfoStackView.snp.makeConstraints { make in
+            make.width.equalTo(170)
+            make.height.equalTo(210)
+            make.top.equalTo(-60)
+            make.centerX.equalToSuperview()
+        }
         
-        NSLayoutConstraint.activate([
-            nameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 15),
-            nameLabel.centerXAnchor.constraint(equalTo: avatarImageView.centerXAnchor),
-            nameLabel.heightAnchor.constraint(equalToConstant: 21)
-        ])
+        sheetImageView.snp.makeConstraints { make in
+            make.bottom.leading.trailing.equalTo(0)
+            make.top.equalTo(snp.bottom).offset(-150)
+
         
-        NSLayoutConstraint.activate([
-            sheetImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0),
-            sheetImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
-            sheetImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
-            sheetImageView.heightAnchor.constraint(equalToConstant: 240)
-            
-        ])
+        }
         
-        NSLayoutConstraint.activate([
-            stripImageView.topAnchor.constraint(equalTo: sheetImageView.topAnchor, constant: 14),
-            stripImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
-    
-            stripImageView.heightAnchor.constraint(equalToConstant: 3),
-            stripImageView.widthAnchor.constraint(equalToConstant: 50)
-        ])
+        stripImageView.snp.makeConstraints { make in
+            make.top.equalTo(sheetImageView.snp.top).offset(14)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(3)
+            make.width.equalTo(50)
+        }
         
-        NSLayoutConstraint.activate([
-            someView.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 100),
-            someView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            someView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -50),
-            someView.heightAnchor.constraint(equalToConstant: 130),
-            someView.widthAnchor.constraint(equalToConstant: 130)
-        ])
+        someView.snp.makeConstraints { make in
+            make.top.equalTo(userInfoStackView.snp.bottom).offset(30)
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(sheetImageView.snp.top).offset(-50)
+            make.height.width.equalTo(130)
+        }
+
+        percentSpeaking.snp.makeConstraints { make in
+            make.centerX.equalTo(someView.snp.centerX).offset(65)
+            make.centerY.equalTo(someView.snp.centerY).offset(0)
+        }
         
-        NSLayoutConstraint.activate([
-            percentSpeaking.centerXAnchor.constraint(equalTo: someView.centerXAnchor, constant: 65),
-            percentSpeaking.centerYAnchor.constraint(equalTo: someView.centerYAnchor, constant: 0),
-            
-            percentListening.centerXAnchor.constraint(equalTo: percentSpeaking.centerXAnchor, constant: -30),
-            percentListening.centerYAnchor.constraint(equalTo: percentSpeaking.centerYAnchor, constant: 55),
-            
-            percentReading.centerXAnchor.constraint(equalTo: someView.centerXAnchor, constant: -55),
-            percentReading.centerYAnchor.constraint(equalTo: someView.centerYAnchor, constant: 33)
-            
-        ])
+        percentListening.snp.makeConstraints { make in
+            make.centerX.equalTo(percentSpeaking.snp.centerX).offset(-30)
+            make.centerY.equalTo(percentSpeaking.snp.centerY).offset(55)
+        }
+
+        percentReading.snp.makeConstraints { make in
+            make.centerX.equalTo(someView.snp.centerX).offset(-55)
+            make.centerY.equalTo(someView.snp.centerY).offset(33)
+        }
         
+        nameSpeaking.snp.makeConstraints { make in
+            make.top.equalTo(someView.snp.top).offset(-5)
+            make.trailing.equalTo(someView.snp.leading).offset(25)
+        }
         
-        NSLayoutConstraint.activate([
-            nameSpeaking.topAnchor.constraint(equalTo: someView.topAnchor, constant: -5),
-            nameSpeaking.trailingAnchor.constraint(equalTo: someView.leadingAnchor, constant: 25),
-            
-            nameListening.topAnchor.constraint(equalTo: someView.centerYAnchor, constant: 30),
-            nameListening.leadingAnchor.constraint(equalTo: someView.trailingAnchor, constant: 35),
-            
-            nameReading.trailingAnchor.constraint(equalTo: someView.leadingAnchor, constant: -25),
-            nameReading.topAnchor.constraint(equalTo: someView.centerYAnchor, constant: 15)
-            
-        ])
+        nameListening.snp.makeConstraints { make in
+            make.top.equalTo(someView.snp.centerY).offset(30)
+            make.leading.equalTo(someView.snp.trailing).offset(35)
+        }
         
-        NSLayoutConstraint.activate([
-            centerLabel.centerXAnchor.constraint(equalTo: someView.centerXAnchor, constant: -10),
-            centerLabel.centerYAnchor.constraint(equalTo: someView.centerYAnchor, constant: 0),
-            
-            centerImageView.leadingAnchor.constraint(equalTo: centerLabel.trailingAnchor, constant: 5),
-            centerImageView.centerYAnchor.constraint(equalTo: someView.centerYAnchor, constant: 0)
-            
-        ])
+        nameReading.snp.makeConstraints { make in
+            make.top.equalTo(someView.snp.centerY).offset(15)
+            make.trailing.equalTo(someView.snp.leading).offset(-25)
+        }
         
+        centerLabelStackView.snp.makeConstraints { make in
+            make.centerX.equalTo(someView.snp.centerX)
+            make.centerY.equalTo(someView.snp.centerY)
+        }
     }
 }
