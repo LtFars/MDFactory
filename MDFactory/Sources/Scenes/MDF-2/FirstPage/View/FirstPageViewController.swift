2f69168ed0028e02b2afd9d047cded0259f8b1c2
Viktor <viktopiniaginya@gmail.com>
2022-09-20 16:51:24 +0300

    add subheadlineLabel and set layouts
    Add description to welcomeLabel method, add subheadlineLabel and set all view's layout for all iphones screens

--- MDFactory/Sources/Scenes/MDF-2/FirstPage/View/FirstPageViewController.swift
+++ MDFactory/Sources/Scenes/MDF-2/FirstPage/View/FirstPageViewController.swift
@@ -25,8 +25,28 @@ class FirstPageViewController: UIViewController {
         label.getToWelcomeLabelText(firstPartOfText: Strings.firstPartOfWelcomeText,
                                     secondPartOfText: Strings.secondPartOfWelcomeText,
                                     fontSize: Metrics.welcomeTextSize)
+        
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.adjustsFontSizeToFitWidth = true
+        
+        return label
+    }()
+    
+    private lazy var subheadlineWelcomeLabel: UILabel = {
+        let label = UILabel()
+        
+        var paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = Metrics.subheadlineWelcomeTextLineHeight
+        label.attributedText = NSMutableAttributedString(string: Strings.subheadlineWelcomeText,
+                                                         attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle,
+                                                                      NSAttributedString.Key.font: UIFont(name: "RedHatDisplay-Medium",
+                                                                                                          size: Metrics.subheadlineWelcomeTextSize) ?? UIFont.systemFont(ofSize: Metrics.subheadlineWelcomeTextSize)])
+        
+        label.textColor = .gray
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.textAlignment = .center
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
     }()
@@ -44,6 +64,7 @@ class FirstPageViewController: UIViewController {
     private func setupHierarchy() {
         view.addSubview(logoImageView)
         view.addSubview(welcomeLabel)
+        view.addSubview(subheadlineWelcomeLabel)
     }
     
     private func setupLayout() {
@@ -59,6 +80,12 @@ class FirstPageViewController: UIViewController {
             make.top.equalTo(logoImageView.snp.bottom).inset(Metrics.welcomeTextInsetToTop)
             make.centerX.equalToSuperview()
         }
+        
+        subheadlineWelcomeLabel.snp.makeConstraints { make in
+            make.top.equalTo(welcomeLabel.snp.bottom).offset(Metrics.subheadlineWelcomeTextTopOffset)
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview().dividedBy(Metrics.subheadlineWelcomeTextWidthRatio)
+        }
     }
     
     private func setupView() {
@@ -71,14 +98,20 @@ extension FirstPageViewController {
     enum Metrics {
         static let logoHeightRatio: CGFloat = 542 / 375
         
-        static let welcomeTextSize: CGFloat = 28
-        static let welcomeTextInsetToTop: CGFloat = 140
+        static let welcomeTextSize: CGFloat = 28 * UIScreen.main.bounds.height / 928
+        static let welcomeTextInsetToTop: CGFloat = 28 * 928 / UIScreen.main.bounds.height + 120
         
+        static let subheadlineWelcomeTextSize: CGFloat = 16 * UIScreen.main.bounds.height / 928
+        static let subheadlineWelcomeTextLineHeight: CGFloat = 1.17 * UIScreen.main.bounds.height / 928
+        static let subheadlineWelcomeTextTopOffset: CGFloat = 12 * UIScreen.main.bounds.height / 928
+        static let subheadlineWelcomeTextWidthRatio: CGFloat = 1.3 * 428 / UIScreen.main.bounds.width
+
     }
     
     enum Strings {
         static let firstPartOfWelcomeText: String = "MobDevFactory"
         static let secondPartOfWelcomeText: String = "App"
         
+        static let subheadlineWelcomeText: String = "Добро пожаловать! Освой профессию iOS разработчика на Swift с нуля до Junior за 6 месяцев вместе с нами."
     }
 }
