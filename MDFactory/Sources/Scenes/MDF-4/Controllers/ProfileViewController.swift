10111dd87916031feec625fd72c14fc9efbbb471
Andrik <masmay.pl@gmail.com>
2022-10-12 18:23:31 +0200

    add collectionViewController

--- MDFactory/Sources/Scenes/MDF-4/Controllers/ProfileViewController.swift
+++ MDFactory/Sources/Scenes/MDF-4/Controllers/ProfileViewController.swift
@@ -10,7 +10,7 @@ import SnapKit
 
 class ProfileViewController: UIViewController {
     
-    private let achievements = AchievementsModel.mocks
+//    private let achievements = AchievementsModel.mocks
     private let sheetProfileView = SheetProfileView()
     
     private let defaultHeight: CGFloat = UIScreen.main.bounds.height / 3.5
@@ -23,10 +23,18 @@ class ProfileViewController: UIViewController {
     private lazy var sheetImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9647058824, blue: 0.9882352941, alpha: 1)
-        imageView.layer.cornerRadius = MetricConstraints.cornerRadiusSheetImage
+        imageView.layer.cornerRadius = Metric.cornerRadiusView
         imageView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
         imageView.clipsToBounds = true
         return imageView
+    }()
+    
+    private lazy var testButton: UIButton = {
+        var button = UIButton()
+        button.setTitle("Press", for: .normal)
+        button.backgroundColor = .green
+        button.addTarget(self, action: #selector(setSheet), for: .touchUpInside)
+        return button
     }()
     
     private lazy var stripImageView: UIImageView = {
@@ -43,37 +51,48 @@ class ProfileViewController: UIViewController {
         return name
     }()
     
-    private lazy var achievementsCollectionView: UICollectionView = {
-        let collection = UICollectionView(frame: .zero,
-                                          collectionViewLayout:  createLayout())
-        collection.backgroundColor = .clear
-        collection.delegate = self
-        collection.dataSource = self
-        collection.register(ProfileCollectionViewCell.self,
-                            forCellWithReuseIdentifier: ProfileCollectionViewCell.identifier)
-        collection.isScrollEnabled = true
-        return collection
-    }()
+//    private lazy var achievementsCollectionView: UICollectionView = {
+//        let collection = UICollectionView(frame: .zero,
+//                                          collectionViewLayout:  createLayout())
+//        collection.backgroundColor = .clear
+//        collection.delegate = self
+//        collection.dataSource = self
+//        collection.register(ProfileCollectionViewCell.self,
+//                            forCellWithReuseIdentifier: ProfileCollectionViewCell.identifier)
+//        collection.isScrollEnabled = true
+//        return collection
+//    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .clear
         setupHierarchy()
         setupLoyaut()
-        setupPanGesture()
+//        setupPanGesture()
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        achievementsCollectionView.frame = view.bounds
-    }
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+////        achievementsCollectionView.frame = view.bounds
+//    }
     
     private func setupHierarchy() {
         view.addSubview(sheetProfileView)
-        view.addSubview(sheetImageView)
-        view.addSubview(stripImageView)
-        sheetImageView.addSubview(achievementsCollectionView)
-        sheetImageView.addSubview(achievementsNameLabel)
+        view.addSubview(testButton)
+    
+//        view.addSubview(sheetImageView)
+//        view.addSubview(stripImageView)
+//        sheetImageView.addSubview(achievementsCollectionView)
+//        sheetImageView.addSubview(achievementsNameLabel)
+    }
+    
+    @objc func setSheet() {
+        
+        let file = AchievementsViewController()
+        if let sheet = file.sheetPresentationController {
+            sheet.detents = [.large(), .medium()]
+        }
+        present(file, animated: true)
     }
     
     private func createLayout() -> UICollectionViewLayout {
@@ -117,27 +136,32 @@ class ProfileViewController: UIViewController {
             make.bottom.leading.trailing.equalTo(MetricConstraints.sheetConstraintsLeadingBottomTrailing)
         }
         
-        achievementsCollectionView.snp.makeConstraints { make in
-            make.trailing.leading.bottom.equalTo(0)
-            make.top.equalTo(MetricConstraints.achievementsCollectionTop)
-            
-        }
-        
-        sheetImageView.snp.makeConstraints { make in
-            make.bottom.leading.trailing.equalTo(MetricConstraints.sheetConstraintsLeadingBottomTrailing)
-        }
-        
-        stripImageView.snp.makeConstraints { make in
-            make.top.equalTo(sheetImageView.snp.top).offset(MetricConstraints.stripTop)
+        testButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.height.equalTo(MetricConstraints.stripHeight)
-            make.width.equalTo(MetricConstraints.stripWidth)
+            make.centerY.equalToSuperview()
         }
         
-        achievementsNameLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(MetricConstraints.achievementsNameTop)
-        }
+//        achievementsCollectionView.snp.makeConstraints { make in
+//            make.trailing.leading.bottom.equalTo(0)
+//            make.top.equalTo(MetricConstraints.achievementsCollectionTop)
+//
+//        }
+        
+//        sheetImageView.snp.makeConstraints { make in
+//            make.bottom.leading.trailing.equalTo(MetricConstraints.sheetConstraintsLeadingBottomTrailing)
+//        }
+//
+//        stripImageView.snp.makeConstraints { make in
+//            make.top.equalTo(sheetImageView.snp.top).offset(MetricConstraints.stripTop)
+//            make.centerX.equalToSuperview()
+//            make.height.equalTo(MetricConstraints.stripHeight)
+//            make.width.equalTo(MetricConstraints.stripWidth)
+//        }
+        
+//        achievementsNameLabel.snp.makeConstraints { make in
+//            make.centerX.equalToSuperview()
+//            make.top.equalTo(MetricConstraints.achievementsNameTop)
+//        }
         
         containerViewHeightConstraint = sheetImageView.heightAnchor.constraint(equalToConstant: defaultHeight)
         
@@ -145,56 +169,54 @@ class ProfileViewController: UIViewController {
         containerViewBottomConstraint?.isActive = true
     }
     
-    private func setupPanGesture() {
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(gesture:)))
-        panGesture.minimumNumberOfTouches = 1
-        panGesture.delaysTouchesBegan = false
-        panGesture.delaysTouchesEnded = false
-        view.addGestureRecognizer(panGesture)
-    }
+//    private func setupPanGesture() {
+//        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(gesture:)))
+//        panGesture.minimumNumberOfTouches = 1
+//        panGesture.delaysTouchesBegan = false
+//        panGesture.delaysTouchesEnded = false
+//        view.addGestureRecognizer(panGesture)
+//    }
     
-    private func animateContainerHeight(_ height: CGFloat) {
-        UIView.animate(withDuration: 0.4) {
-            self.containerViewHeightConstraint?.constant = height
-            self.view.layoutIfNeeded()
-        }
-        currentContainerHeight = height
-    }
+//    private func animateContainerHeight(_ height: CGFloat) {
+//        UIView.animate(withDuration: 0.4) {
+//            self.containerViewHeightConstraint?.constant = height
+//            self.view.layoutIfNeeded()
+//        }
+//        currentContainerHeight = height
+//    }
     
-    @objc func handlePanGesture(gesture: UIPanGestureRecognizer) {
-        
-        let translation = gesture.translation(in: sheetImageView)
-        let isDraggingDown = translation.y > 0
-        let newHeight = currentContainerHeight - translation.y
-        switch gesture.state {
-            
-        case .changed:
-            if newHeight < maximumContainerHeight {
-                containerViewHeightConstraint?.constant = newHeight
-            }
-            
-        case .ended:
-            if newHeight < defaultHeight {
-                animateContainerHeight(defaultHeight)
-            }
-            
-            else if newHeight < maximumContainerHeight && isDraggingDown {
-                animateContainerHeight(defaultHeight)
-                sheetImageView.isUserInteractionEnabled = false
-            }
-            
-            else if newHeight > defaultHeight && !isDraggingDown {
-                animateContainerHeight(maximumContainerHeight)
-                sheetImageView.isUserInteractionEnabled = true
-            }
-            
-        default: break
-        }
-    }
+//    @objc func handlePanGesture(gesture: UIPanGestureRecognizer) {
+//
+//        let translation = gesture.translation(in: sheetImageView)
+//        let isDraggingDown = translation.y > 0
+//        let newHeight = currentContainerHeight - translation.y
+//        switch gesture.state {
+//
+//        case .changed:
+//            if newHeight < maximumContainerHeight {
+//                containerViewHeightConstraint?.constant = newHeight
+//            }
+//
+//        case .ended:
+//            if newHeight < defaultHeight {
+//                animateContainerHeight(defaultHeight)
+//            }
+//
+//            else if newHeight < maximumContainerHeight && isDraggingDown {
+//                animateContainerHeight(defaultHeight)
+//                sheetImageView.isUserInteractionEnabled = false
+//            }
+//
+//            else if newHeight > defaultHeight && !isDraggingDown {
+//                animateContainerHeight(maximumContainerHeight)
+//                sheetImageView.isUserInteractionEnabled = true
+//            }
+//
+//        default: break
+//        }
+//    }
+//
     
-    @objc func tabActiohButton() {
-        dismiss(animated: true)
-    }
     
     enum MetricConstraints {
         static var sheetConstraintsLeadingBottomTrailing: CGFloat = 0
@@ -202,7 +224,6 @@ class ProfileViewController: UIViewController {
         static var stripWidth: CGFloat = UIScreen.main.bounds.width / 9
         static var stripTop: CGFloat = 14
         static var achievementsNameTop: CGFloat = 39
-        static var cornerRadiusSheetImage: CGFloat = 40
         static var cornerRadiusStripImage: CGFloat = 2
         static var achievementsCollectionTop: CGFloat = 50
     }
@@ -213,38 +234,38 @@ class ProfileViewController: UIViewController {
     }
 }
 
-extension ProfileViewController: UICollectionViewDelegate {
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        numberOfItemsInSection section: Int) -> Int {
-        return achievements.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        didSelectItemAt indexPath: IndexPath) {
-        collectionView.deselectItem(at: indexPath, animated: true)
-        
-        let cover = achievements[indexPath.row]
-        print("selected \(cover.name)")
-    }
-}
-
-extension ProfileViewController: UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        cellForItemAt indexPath: IndexPath
-    ) -> UICollectionViewCell {
-        
-        guard let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: ProfileCollectionViewCell.identifier,
-            for: indexPath
-        ) as? ProfileCollectionViewCell else {
-            return UICollectionViewCell()
-            
-        }
-        
-        let model = achievements[indexPath.row]
-        cell.configure(with: model)
-        return cell
-    }
-}
+//extension ProfileViewController: UICollectionViewDelegate {
+//    
+//    func collectionView(_ collectionView: UICollectionView,
+//                        numberOfItemsInSection section: Int) -> Int {
+//        return achievements.count
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView,
+//                        didSelectItemAt indexPath: IndexPath) {
+//        collectionView.deselectItem(at: indexPath, animated: true)
+//        
+//        let cover = achievements[indexPath.row]
+//        print("selected \(cover.name)")
+//    }
+//}
+//
+//extension ProfileViewController: UICollectionViewDataSource {
+//    
+//    func collectionView(_ collectionView: UICollectionView,
+//                        cellForItemAt indexPath: IndexPath
+//    ) -> UICollectionViewCell {
+//        
+//        guard let cell = collectionView.dequeueReusableCell(
+//            withReuseIdentifier: ProfileCollectionViewCell.identifier,
+//            for: indexPath
+//        ) as? ProfileCollectionViewCell else {
+//            return UICollectionViewCell()
+//            
+//        }
+//        
+//        let model = achievements[indexPath.row]
+//        cell.configure(with: model)
+//        return cell
+//    }
+//}
---
+++ MDFactory/Sources/Scenes/MDF-4/Enums/Metric.swift
@@ -0,0 +1,12 @@
+//
+//  Metric.swift
+//  MDFactory
+//
+//  Created by Andrei Maskal on 12/10/2022.
+//
+
+import UIKit
+
+enum Metric {
+    static var cornerRadiusView: CGFloat = 40
+}
