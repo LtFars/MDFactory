b432b7e8b1b9755040841a0a2214d71ad609038b
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-10-16 20:08:44 +0300

    add SettingPageVC and folders

---
+++ MDFactory/Sources/Scenes/MDF-5/View/SettingPageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingPageViewController.swift
+//  MDFactory
+//
+//  Created by Dmitry Dorodniy on 16.10.2022.
+//
+
+import UIKit
+
+class SettingPageViewController: UIViewController {
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
