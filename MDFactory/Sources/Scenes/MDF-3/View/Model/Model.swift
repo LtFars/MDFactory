bc965aa827e4a882585481cc047207b6fb003a5c
alexey-z-m <alexey.panda@gmail.com>
2022-10-18 10:26:00 +0300

    add collectionViewCellGrid

--- MDFactory/Sources/Scenes/MDF-3/View/Model/Model.swift
+++ MDFactory/Sources/Scenes/MDF-3/View/Model/Model.swift
@@ -16,7 +16,7 @@ struct Model {
             ItemForMain(title: "Rate", description: "Превью рейтинга студента",  image: UIImage(named: "rate")),
             ItemForMain(title: "Testing", description: "Превью результатов тестирования",  image: UIImage(named: "tests")),
             ItemForMain(title: "WebView", description: "Полезные статьи",  image: UIImage(named: "webView")),
-            //ItemForMain(title: "Test", description: "Test test test",  image: UIImage(systemName: "person"))
+           // ItemForMain(title: "Test", description: "Test test test",  image: UIImage(systemName: "person"))
         ]
     }
 }
