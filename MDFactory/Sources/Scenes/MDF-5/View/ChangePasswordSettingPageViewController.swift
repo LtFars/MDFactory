dd21108c80da2ee6eb03fd092d74ec1f3d8c56bd
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-10-23 11:24:43 +0300

    add buttons to change password VC

--- MDFactory/Sources/Scenes/MDF-5/View/ChangePasswordSettingPageViewController.swift
+++ MDFactory/Sources/Scenes/MDF-5/View/ChangePasswordSettingPageViewController.swift
@@ -18,6 +18,31 @@ class ChangePasswordSettingPageViewController: UIViewController {
         return textField
     }()
 
+    private lazy var passwordAgainTextField: CustomLoginTextField = {
+        let textField = CustomLoginTextField(labelText: "Новый пароль ещё раз")
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.autocapitalizationType = .none
+        textField.returnKeyType = UIReturnKeyType.next
+        textField.tag = 0
+        return textField
+    }()
+
+    private lazy var changePasswordButton: UIButton = {
+        let button = MainCustomButton(cornerRadius: 16 * UIScreen.main.bounds.height / 812)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.setTitle("изменить пароль", for: .normal)
+        //        button.addTarget(self, action: #selector(logOutButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var backButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(named: "back")
+        button.setImage(image, for: .normal)
+        button.addTarget(self, action: #selector(backAction), for: .touchUpInside)
+        return button
+    }()
+
     private lazy var buttonStack: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
@@ -33,20 +58,32 @@ class ChangePasswordSettingPageViewController: UIViewController {
         setupLayout()
     }
 
+    @objc private func backAction() {
+        navigationController?.popViewController(animated: true)
+    }
+
     private func setupView() {
 
         view.backgroundColor = .systemGray6
         navigationItem.title = "Изменить пароль"
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
 
         view.addSubview(buttonStack)
-        buttonStack.addArrangedSubview(passwordTextField)
+
+        let buttonStackSubview = [
+            passwordTextField,
+            passwordAgainTextField,
+            changePasswordButton
+        ]
+
+        buttonStackSubview.forEach {
+            buttonStack.addArrangedSubview($0)
+        }
     }
 
     private func setupLayout() {
         passwordTextField.snp.makeConstraints { make in
-//            make.top.equalTo(view.safeAreaLayoutGuide).offset(30)
-//            make.leading.trailing.equalTo(view).inset(20)
             make.height.equalTo(58 * UIScreen.main.bounds.height / 812)
         }
 
