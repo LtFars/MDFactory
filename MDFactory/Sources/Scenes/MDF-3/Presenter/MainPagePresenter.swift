bd025d8a18446553fe1b1245073f4d2385c37a40
alexey-z-m <alexey.panda@gmail.com>
2022-10-24 12:44:56 +0300

    add presenter

---
+++ MDFactory/Sources/Scenes/MDF-3/Presenter/MainPagePresenter.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+protocol MainPagePresenterProtocol {
+    var model: [ItemForMain] { get }
+    func getItemsCount() -> Int
+    func getItemTitle(index: Int) -> String
+    func getItemDescription(index: Int) -> String
+    func getItemImage(index: Int) -> UIImage?
+    func getItemHandler(index: Int)
+    
+}
+
+class MainPagePresenter: MainPagePresenterProtocol {
+    var model: [ItemForMain] = MainPageModel().itemForCollection()
+    
+    func getItemsCount() -> Int {
+        return model.count
+    }
+    
+    func getItemTitle(index: Int) -> String {
+        return model[index].title
+    }
+    
+    func getItemDescription(index: Int) -> String {
+        return model[index].description
+    }
+    
+    func getItemImage(index: Int) -> UIImage? {
+        return model[index].image
+    }
+    
+    func getItemHandler(index: Int) {
+        var _ = model[index].handler
+    }
+}
