04d3da8e3261389cbcf3fdf10381b17adee94503
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-12-13 14:54:12 +0300

    add auto login feather

--- MDFactory/Sources/Application/SceneDelegate.swift
+++ MDFactory/Sources/Application/SceneDelegate.swift
@@ -11,15 +11,41 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    private var rootVC: UIViewController = FirstPageViewController()
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let login = FirebaseService().userName
         
-       let navigationController = UINavigationController(rootViewController: FirstPageViewController())
-        
+        do {
+            if try SecureStorage.isExists(userName: login) {
+                let password = try SecureStorage.readPassword(userName: login)
+                print("autoLogin user: \(login), password \(password)")
+                autoLogin(login: login, password: password)
+            } else {
+                print("No user data in keychain")
+            }
+        } catch {
+            print(error)
+        }
+
+        let navigationController = UINavigationController(rootViewController: rootVC)
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
+    }
+
+    private func autoLogin(login: String, password: String) {
+        FirebaseService().signIn(email: login, password: password) { [weak self] result in
+            switch result {
+            case .success:
+                self?.window?.switchRootViewController(to: MainTabBarController())
+            case .failure(_):
+                break
+            }
+        }
     }
 }
 
